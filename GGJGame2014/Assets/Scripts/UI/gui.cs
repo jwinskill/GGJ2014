addf689ddc0960205a80746629daaf18bc18519b
Patrick Morgan <p.w.morgan@gmail.com>
2014-01-25 17:29:04 -0800

    Working on GUI script.

--- GGJGame2014/Assets/Scripts/UI/gui.cs
+++ GGJGame2014/Assets/Scripts/UI/gui.cs
@@ -8,19 +8,25 @@ public class gui : MonoBehaviour {
 	public GameObject atmoBar;
 	public GameObject waterBar;
 
-	void OnGUI () {
-		// Draw Meters
+	public planet targetplanet;
 
-			// Atmosphere 
-			// Atmosphere Breathability
-			// Radiation
-			// Temperature
-			// Life
-			
+	private float lowerThreshold = -0.1f; // Lowest X value of UI
+	private float upperThreshold = 1.75f; // Highest X value of UI
+
+	private float threshold;
+
+	void Start () {
+		threshold = upperThreshold - lowerThreshold;
+	}
+
+	void Update () {
+		// Stability
 
 
-			// Core Temperature
-			// Ozone Layer
-			//
+		// Temperature
+
+		// Atmosphere
+
+		// Water
 	}
 }
